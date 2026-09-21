6015adece9a5e1312e757336967a5b4e3b1445d7
Iván Saavedra <ivan.saavedra@people-media.com.mx>
2026-09-21 08:50:38 -0600

    feature exception handling

---
+++ src/main/java/com/customer_service/exception/ApiException.java
@@ -0,0 +1,24 @@
+package com.customer_service.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiException extends RuntimeException{
+    
+    private HttpStatus status;
+    
+    public ApiException(HttpStatus status, String message) {
+		super(message);
+		this.status = status;
+	}
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+
+    
+
+}
